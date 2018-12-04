ea610926b368fbff193234134f0a4e837476b548
MarceArratiaF <marratiaf@hotmail.com>
2018-12-03 21:24:39 -0300

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,62 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+    encode: (strng,offset) => {
+//variable para almacenar el código ascii de la letra
+let ascii=0;
+let textoCodificado="";
+
+//bucle for para recorrer letras del texto una por una
+for(let i=0;i<strng.length;i++){
+    let formula;
+    //cuando usuario clickea espacio, = asignar algo,== igual
+    //evalua si no es un espacio
+    if(strng[i].charCodeAt()==32){
+        formula=32;
+    }else{
+         //si no es espacio es una letra por lo tanto codifica
+        ascii=strng[i].toUpperCase().charCodeAt(0);
+        //transforma ascii a numero entero (parseInt)
+        formula=((parseInt(ascii)-65+parseInt(offset))%26)+65;
+
+    }
+
+//almacenar la letra en ascii
+
+
+
+textoCodificado=textoCodificado+String.fromCharCode(formula);
+    }
+    return textoCodificado.toUpperCase();
+},
+
+
+    decode: (srngD,offsetD) => {
+        //variable para almacenar el código ascii de la letra
+let ascii=0;
+let textoCodificado="";
+
+//bucle for para recorrer letras del texto una por una
+for(let i=0;i<srngD.length;i++){
+
+let formula;
+
+
+if(srngD[i].charCodeAt()==32){
+    formula=32;
+}
+else{
+    //transforma ascii a numero entero (parseInt)
+//se resta el desplazamiento para decodificar
+ascii=srngD[i].toUpperCase().charCodeAt(0);
+formula=((parseInt(ascii)-65-parseInt(offsetD))%26)+65;
+}
+
+
+
+textoCodificado=textoCodificado+String.fromCharCode(formula);
+    }
+    return textoCodificado.toUpperCase();
+    
+
+
+    }
+   };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-/* Acá va tu código */
+/* Acá va tu código manejo del dom*/
+function codificar(){
+   //guardar valor de cuadro que usuario digita
+   let strng=document.getElementById('CuadroTexto').value;
+   //guardo desplazamiento que elige usuario
+   let offset= document.getElementById('desplazamiento').value;
+   //guardar todo el contenido de cipher
+   console.log(offset,strng);
+   let paso= cipher.encode(strng,offset);
+
+   //    root.innerHTML = cipher.encode(texto,offset)
+   document.getElementById('CuadroSalidaC').innerHTML=paso;
+}
+function decodificar(){
+   //guadar valor de cuadro que usuario digita para descifrar
+   let strngD= document.getElementById('CuadroTexto').value;
+   //el espacio de desplazamiento elegido por el usuario
+   let offsetD=document.getElementById('desplazamiento').value;
+   //guardar contenido de cipher
+   let pasoD= cipher.decode(strngD,offsetD);
+   console.log(pasoD);
+   //root.innerHTML = cipher.decode(texto,offset)
+   document.getElementById('CuadroSalidaD').innerHTML=pasoD;
+
+}
+//e carga como offset y strng, recibe informacion
+function mayuscula(e){
+   e.value=e.value.toUpperCase();
+  //evalúa codigo ascii de tecla ingresada  Ñ para cambiarla por N
+   if(e.value.charCodeAt()==209){
+e.value="N";
+
+   }
+   //
+
+}
+
+
+    
+
+
