4e2f2da20ecafe257f5cb49cfa036bdc927c3579
MarceArratiaF <marratiaf@hotmail.com>
2018-12-04 22:29:20 -0300

    Diseño de pagina de #$%&&%%13431@@

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@
 function codificar(){
    //guardar valor de cuadro que usuario digita
    let strng=document.getElementById('CuadroTexto').value;
+   
    //guardo desplazamiento que elige usuario
    let offset= document.getElementById('desplazamiento').value;
+   
    //guardar todo el contenido de cipher
   
    let paso= cipher.encode(strng,offset);
 
    //    root.innerHTML = cipher.encode(texto,offset)
-   document.getElementById('divcodificado').style="display:block;float: center; ";
+
+   //document.getElementById('divcodificado').style="display:block;float: center; ";
    document.getElementById('CuadroSalidaC').innerHTML=paso;
 }
 function decodificar(){
@@ -19,7 +22,8 @@ function decodificar(){
    let offsetD=document.getElementById('desplazamiento').value;
    //guardar contenido de cipher
    let pasoD= cipher.decode(strngD,offsetD);
-   document.getElementById('divdecodificado').style="display:block;float: center; ";
+
+//   document.getElementById('divdecodificado').style="display:block;float: center; ";
    //root.innerHTML = cipher.decode(texto,offset)
    document.getElementById('CuadroSalidaD').innerHTML=pasoD;
 
