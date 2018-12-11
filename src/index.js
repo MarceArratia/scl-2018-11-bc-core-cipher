c1dee0683920d5ee29fb24c160c25db3cc50f369
MarceArratiaF <marratiaf@hotmail.com>
2018-12-10 21:40:20 -0300

    eliminando caracteres especiales

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ function codificar(){
    //guardo desplazamiento que elige usuario
    let offset= document.getElementById('desplazamiento').value;
    let paso;
-     //guardar todo el contenido de cipher
+     //reemplazar caracteres especiales por nada
+     strng=strng.replace(/[0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,'');
+     //actualiza el texto ingresado sin caracteres especiales, 
+     document.getElementById('CuadroTexto').value=strng;
+   //cambia los números negativos por números positivos en el desplazamiento
    if (offset > 0 ){
+      //guardar todo el contenido de cipher
        paso=cipher.encode(strng,offset);
       }else{
         offset=offset*-1;
@@ -24,6 +29,10 @@ function decodificar(){
    //guadar valor de cuadro que usuario digita para descifrar
    let strngD= document.getElementById('CuadroTexto').value;
    let pasoD;
+   //reemplazar caracteres especiales por nada
+   strngD=strngD.replace(/[0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,'');
+     //actualiza el texto ingresado sin caracteres especiales, 
+     document.getElementById('CuadroTexto').value=strngD;
    //el espacio de desplazamiento elegido por el usuario
    let offsetD=document.getElementById('desplazamiento').value;
     //guardar contenido de cipher
